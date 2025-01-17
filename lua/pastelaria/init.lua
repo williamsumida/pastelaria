e6d414bbbb4067a70ce3ea2e60e875643e525abb
William Sumida <william.h.sumida@gmail.com>
2025-01-17 18:10:31 -0300

    batman

---
+++ lua/pastelaria/init.lua
@@ -0,0 +1,32 @@
+local M = {}
+
+function SetAllHighlights(fg, bg)
+	-- Get the list of all highlight groups
+	local hl_groups = vim.fn.getcompletion("", "highlight")
+
+	-- Set the same colors for all groups
+	for _, group in ipairs(hl_groups) do
+		vim.api.nvim_set_hl(0, group, { fg = fg, bg = bg })
+	end
+end
+
+-- Call the function with your desired colors
+
+-- paleta de cores
+-- default: #FFF6E3
+-- comentarios: #B7B7B7
+--
+function M.setup()
+	SetAllHighlights("#FFF6E3", "#000000") -- White text on black background
+	vim.cmd("highlight clear")
+	vim.cmd("syntax reset")
+
+	SetAllHighlights("#FFF6E3", "#000000") -- White text on black background
+	vim.api.nvim_set_hl(0, "Normal", { fg = "#FFF6E3", bg = "#000000" })
+	vim.api.nvim_set_hl(0, "Comment", { fg = "#B7B7B7", bg = "#000000" })
+	vim.api.nvim_set_hl(0, "Keyword", { fg = "#FFF6E3", bg = "#000000" })
+	vim.api.nvim_set_hl(0, "Function", { fg = "#FFF6E3", bg = "#000000" })
+	vim.g.colors_name = "first-theme"
+end
+
+return M
